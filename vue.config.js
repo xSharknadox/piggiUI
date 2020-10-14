1d8fc206718f37b24478cd82124497c826182062
Igarek <chernuha9544@gmail.com>
2020-10-14 17:28:36 +0300

    add redirect on api

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     "transpileDependencies": [
         "vuetify"
-    ]
+    ],
+    devServer: {
+        port:9000,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080', // this configuration needs to correspond to the Spring Boot backends' application.properties server.port
+                ws: true,
+                changeOrigin: true
+            }
+        }
+    },
 }
